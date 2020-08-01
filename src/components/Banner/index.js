d692057c4ec19adb54410bafdbc638bca807d9a4
walacefex <walacefex01@gmail.com>
2020-08-01 20:23:40 -0300

    banner destop version

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -1,8 +1,30 @@
 import styled from 'styled-components';
 import container from '../../tools/container';
+import Tag from '../Tag';
+import Title from '../Title';
+import Thumb from '../Thumb';
 
-const Banner = styled.section`
-  ${container}
+export const Text= styled.div`
+   padding-top:10rem;
+   max-width:45%;
+  ${Tag}{
+    margin-bottom:40rem;
+
+  }
+  ${Title}{
+    margin-bottom:20rem;
+  }
+
 `;
 
-export default Banner;
+
+export const Banner = styled.section`
+  display:flex;
+  justify-content:space-between;
+  align-items:center;
+  ${container};
+  max-height:70vh;
+  height:100vh;
+
+ 
+`;

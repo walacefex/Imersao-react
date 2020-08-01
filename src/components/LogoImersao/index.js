61a0c45b286960ca4121900dfb60cded7702c101
walacefex <walacefex01@gmail.com>
2020-08-01 17:35:59 -0300

    Componente de imagem do rodapé

---
+++ src/components/LogoImersao/index.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+import logo from '../../assets/img/imersao.svg';
+
+
+const LogoImerdsao = styled.img.attrs({ src:logo, alt:'Logo #ImersãoReact da Alura'})`
+  height:40px;
+  vertical-align:middle;
+`;
+
+export default LogoImerdsao;

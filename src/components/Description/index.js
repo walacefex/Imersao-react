aa8f8df7be2957059e1c1b15b4a69f1a87e97847
walacefex <walacefex01@gmail.com>
2020-08-01 19:29:47 -0300

    Criando o componente de descrição do banner

---
+++ src/components/Description/index.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const Description = styled.p `
+  font-size:18rem;
+  color:var(--color-gray-light);
+  font-weight:300;
+`;
+
+
+export default Description;

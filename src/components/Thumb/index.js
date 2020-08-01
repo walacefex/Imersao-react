331bf4c8e082f88b8a27e60cfb174c982debac92
walacefex <walacefex01@gmail.com>
2020-08-01 20:07:58 -0300

    Crindo o thumbnail da home

---
+++ src/components/Thumb/index.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const Thumb = styled.img`
+  border-radius:4px;
+  border: 4rem solid var(--color-frontend);
+  width:640px;
+`;
+
+
+export default Thumb;

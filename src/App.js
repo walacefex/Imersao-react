e740e39a16902c6d6668720dcb206ebae9ec9a47
walacefex <walacefex01@gmail.com>
2020-08-01 20:08:56 -0300

    Refatoração de código

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import Header from './components/Header';
+import {Header, Wrapper} from './components/Header';
 import Logo from './components/Logo';
 import Button from './components/Button';
 import Footer from './components/Footer';
@@ -9,18 +9,30 @@ import LogoImersao from './components/LogoImersao';
 import Banner from './components/Banner';
 import Tag from './components/Tag';
 import Title from './components/Title';
+import Description from './components/Description';
+import Thumb from './components/Thumb';
+import ImageMB from './assets/img/marco-bruno.PNG';
+
 
 function App() {
   return (
     <>
     <Header>
-     <Logo />
-     <Button>Novo Vídeo</Button>
+
+      <Wrapper>
+         <Logo />
+        <Button>Novo Vídeo</Button>
+      </Wrapper>
     </Header>
 
     <Banner>
       <Tag>Fron-end</Tag>
-      <Title>Curso de React - Marco</Title>
+      <Title>Curso de React - Marco Bruno</Title>
+      <Description>
+        Neste vídeo o Marco ensina a usar o React, junto com o modelo criado da Imersão React da Alura.
+      </Description> 
+
+      <Thumb src={ImageMB} alt="Marco Bruno ensinando importar no React"/>
     </Banner>
 
 

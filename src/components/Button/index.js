73c9840a72eece6fd987e962a18b337ad966cb88
walacefex <walacefex01@gmail.com>
2020-08-01 19:16:41 -0300

    mudando os tamanhos do padding e font

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -2,15 +2,15 @@ import styled from 'styled-components';
 
 
 const Button = styled.button `
-  padding: 14px 35px;
+  padding: 12rem 30rem;
   background-color:var(--color-black-dark);
   border: 2px solid var(--color-gray-light);
   border-radius:4px;
   color: var(--color-gray-light);
-  font-size: 18rem;
+  font-size: 16rem;
   cursor:pointer;
-
-
 `;
+
+
 
 export default Button;

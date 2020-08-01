77336d6bfd48c5341fd7adb6d0fca10b96ecc939
walacefex <walacefex01@gmail.com>
2020-08-01 19:15:08 -0300

    Rotulo do video

---
+++ src/components/Tag/index.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const Tag = styled.h2`
+  display:inline-block;
+  border-radius:4rem;
+  font-size:50rem;
+  background-color:var(--color-frontend);
+  padding:20rem 18rem;
+  color: var(--color-gray-light);
+`
+
+
+export default Tag;
